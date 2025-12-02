6df2c1fa950f969fadb6384e2515af6fd441332f
mubashirsiddique2024-commits <mubashirsiddique2024@gmail.com>
2025-12-03 02:21:35 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,167 @@
+<?php include 'config.php'?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav id="nav">
+        <div class="nav_logo"><h1>DISCOVER</h1><p>PAKISTAN</p></div>
+        <ul class="nav_links">
+            <li><a href="#banner">Home</a></li>
+            <li><a href="#Explore_our_places">Explore our Places</a></li>
+            <li><a href="#our_blogs">Our Blogs</a></li>
+            <li><a href="#comments">visitor</a></li>
+            <li><a href="#FAQ">FAQs</a></li>
+            <li><a href="signin.php" class="signIn">Sign-in</a></li>
+        </ul>
+    </nav>
+    <section class="banner" id="banner">
+        <div class="banner_elems">
+            <h1 class="banner_heading">Pakistan</h1>
+            <p class="banner_para">Experience the warm hospitality and rich traditions of Pakistan. From breathtaking mountains to vibrant cities, every corner tells a story. Discover its diverse culture, historic landmarks, and stunning landscapes. A journey full of adventure and unforgettable memories awaits you.</p>
+        </div>
+    </section>
+
+<section class="Web_font"  id="columns"><h1>wanna come here?</h1><p>whats your purpose</p></section>
+
+    <div class="columns">
+        <a href="https://www.booking.com/booking-home/city/us/van.en-gb.html?aid=1137422&label=van-9Fq3X7qyqpo3IQl2C0P1FQS626027808895%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-342023642120%3Alp9077152%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YUNMGHk8cZwo1oF4AiQZay8&gclid=CjwKCAiAlrXJBhBAEiwA-5pgwmm8ZYy1sKySDNY_HJwO978loI6HZtU9ebmpjAwLP1S4UfeZX6qYsxoCkDoQAvD_BwE&keep_landing=1&sid=10e42e5c24ed89874d4b0c4b76857a45" class="column">
+            <img src="traveller.png" alt="">
+            <h2 class="animate-y">Are you a Traveller ?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt sit distinctio quisquam consectetur temporibus neque? Minus quod dolorum voluptatum itaque!This is the first column.</p>
+        </a>
+        <a href="https://chalayao.com/i-want-custom-tour/?gad_source=1&gad_campaignid=21986036597&gbraid=0AAAAAoJ0bnM6BXt94cX1CKa8xIzlYx2z5&gclid=CjwKCAiAlrXJBhBAEiwA-5pgwtaGr7gZ4cWMnLhtyoZ1Xe8KD2lzzBTbuJwto3V6C2IvRZ0WKiCT2BoCl_kQAvD_BwE" class="column">
+            <img src="tourist.png" alt="">
+            <h2 class="animate-y">Are you a tourist ?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt sit distinctio quisquam consectetur temporibus neque? Minus quod dolorum voluptatum itaque!This is the second column.</p>
+        </a>
+        <a href="https://www.zameen.com/" class="column">
+            <img src="resident.png" alt="">
+            <h2 class="animate-y">Are you a resident ?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt sit distinctio quisquam consectetur temporibus neque? Minus quod dolorum voluptatum itaque!This is the third column.</p>
+        </a>
+    </div>
+
+<section class="Web_font" id="Explore_our_places"><h1>Our Tourist places</h1><p>these are the most famous places</p></section>
+    <div class="Pak_slider" >
+        <div class="slides">
+            <div class="slide"><img class='slide_background' src="../AmazingPakistan/amit-asthana-NpjM5cuI-JE-unsplash.jpg" ><div class="slide_elems"><div class="slide_txt"><h1>Minar-e-Pakistan</h1><p>Minar-e-Pakistan, jo Lahore ke Iqbal Park me waqia hai, Pakistan ki tareekh ka aik bohot ahem aur qabil-e-ehtram yaadgar hai.</p></div><img src="SKU002.jpg" alt=""></div></div>
+            <div class="slide"><img class='slide_background' src="../AmazingPakistan/amit-asthana-NpjM5cuI-JE-unsplash.jpg" ><div class="slide_elems"><div class="slide_txt"><h1>Badshahi Mosque</h1><p>Badshahi Mosque, Lahore me waqia, duniya ki sab se bari aur mashhoor masajid me se aik hai. Yeh masjid 1673 me Mughal Shahenshah Aurangzeb ne tameer karwai thi aur Mughal fun-e-taamir ki shan o azmat ki alamat hai.</p></div><img src="SKU001.jpg" alt=""></div></div>
+            <div class="slide"><img class='slide_background' src="../AmazingPakistan/amit-asthana-NpjM5cuI-JE-unsplash.jpg" ><div class="slide_elems"><div class="slide_txt"><h1>Faisal Mosque</h1><p>Faisal Mosque, Islamabad ke Margalla Hills ke foothills me waqia, Pakistan ki sab se bari aur modern Islamic architecture ki misaal hai. Turkish architect Vedat Dalokay ne is mosque ko design kiya jo traditional dome design se mukhtalif hai</p></div><img src="SKU004.jpg" alt=""></div></div>
+            <div class="slide"><img class='slide_background' src="../AmazingPakistan/amit-asthana-NpjM5cuI-JE-unsplash.jpg" ><div class="slide_elems"><div class="slide_txt"><h1>Mazar-e-Quaid</h1><p>Mazar-e-Quaid, Karachi me waqia, Pakistan ke founder Quaid-e-Azam Muhammad Ali Jinnah ka final resting place hai. Yeh white marble me bana hai aur design me simple lekin dignified hai, jo Quaid ki shakhsiyat aur vision ko reflect karta hai.</p></div><img src="SKU005.jpg" alt=""></div></div>
+            <div class="slide"><img class='slide_background' src="../AmazingPakistan/amit-asthana-NpjM5cuI-JE-unsplash.jpg" ><div class="slide_elems"><div class="slide_txt"><h1>Pakistan Monument</h1><p>'Pakistan Monument, Islamabad me waqia, chaar provinces ki unity ko symbol karta hai. Har petal Punjab, Sindh, Balochistan aur Khyber Pakhtunkhwa ko represent karta hai. Monument ke gird beautiful terrace aur panoramic city view hai. </p></div><img src="SKU006.jpg" alt=""></div></div>
+        </div>
+    </div>
+
+<br>
+
+<div class="Web_font"><h1>more than<div class="dynamic"></div>visitors</h1><p>are come here to enjoy our weekends with the hosipitial peoples</p></div>
+
+
+<section class="Web_font" id="our_blogs"><h1>Our Places</h1><p>that you should have to visit</p></section>
+
+<?php
+    $result = $conn->query('SELECT * FROM famous_places');
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+?>
+<div class="blogs">
+    <div class="blog_elems">
+        <div class="blog_img" style="background-image: url(../AmazingPakistan/<?= $row['image']?>);" ><h1 class="animate-x"><?= $row['name']?></h1></div>
+        <p class="blog_txt"><?= $row['para']?></p>
+    </div>
+</div>
+<?php
+}
+    } else {
+        echo "0 results";
+    }
+    
+?>
+
+
+<div class="Web_font"><h1>Northen Areas</h1><p>Northen areas of Pakistan has huge greenry</p></div>
+
+<?php
+    $result = $conn->query('SELECT * FROM northern_areas');
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+?>
+<div class="blogs">
+    <div class="blog_elems">
+        <div class="blog_img" style="background-image: url(../AmazingPakistan/<?= $row['image']?>);" ><h1 class="animate-x"><?= $row['name']?></h1></div>
+        <p class="blog_txt"><?= $row['para']?></p>
+    </div>
+</div>
+
+<?php
+}
+}else {
+        echo "0 results";
+}
+
+$conn->close();
+?>
+
+
+<div class="blog_elem_links">
+<a href="https://www.britannica.com/place/Pakistan">read more blogs and biography</a>
+<a href="https://en.wikipedia.org/wiki/Culture_of_Pakistan">know about pakistan culture</a>
+</div>
+
+<section class="Web_font" id="comments"><h1>Our visitors</h1><p>what about foregnier says about pakistan </p></section>
+
+<div class="comments">
+    <div class="comment_slider">
+        <div class="comment"><i class="fa-regular fa-user"></i><div class="comment_txt"><h1>Sir Noman</h1><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit laudantium praesentium laborum assumenda officiis quis voluptatem libero porro, dicta delectus asperiores, repellat eum!</p></div></div>
+        <div class="comment"><i class="fa-regular fa-user"></i><div class="comment_txt"><h1>Sir Iman-ul-haq</h1><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit laudantium praesentium laborum assumenda officiis quis voluptatem libero porro, dicta delectus asperiores, repellat eum!</p></div></div>
+        <div class="comment"><i class="fa-regular fa-user"></i><div class="comment_txt"><h1>Sir Nasir</h1><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit laudantium praesentium laborum assumenda officiis quis voluptatem libero porro, dicta delectus asperiores, repellat eum!</p></div></div>
+        <div class="comment"><i class="fa-regular fa-user"></i><div class="comment_txt"><h1>Mubashir</h1><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit laudantium praesentium laborum assumenda officiis quis voluptatem libero porro, dicta delectus asperiores, repellat eum!</p></div></div>
+        <div class="comment"><i class="fa-regular fa-user"></i><div class="comment_txt"><h1>Sir Imran Ul Haq</h1><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit laudantium praesentium laborum assumenda officiis quis voluptatem libero porro, dicta delectus asperiores, repellat eum!</p></div></div>
+        <div class="comment"><i class="fa-regular fa-user"></i><div class="comment_txt"><h1>Adnan uncle</h1><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit laudantium praesentium laborum assumenda officiis quis voluptatem libero porro, dicta delectus asperiores, repellat eum!</p></div></div>
+         <div class="comment"><i class="fa-regular fa-user"></i><div class="comment_txt"><h1>Sir Tahir Talib</h1><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit laudantium praesentium laborum assumenda officiis quis voluptatem libero porro, dicta delectus asperiores, repellat eum!</p></div></div>
+        <div class="comment"><i class="fa-regular fa-user"></i><div class="comment_txt"><h1>Sir Atta Ur Rehman</h1><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit laudantium praesentium laborum assumenda officiis quis voluptatem libero porro, dicta delectus asperiores, repellat eum!</p></div></div>
+        <div class="comment"><i class="fa-regular fa-user"></i><div class="comment_txt"><h1>Sir Noman</h1><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit laudantium praesentium laborum assumenda officiis quis voluptatem libero porro, dicta delectus asperiores, repellat eum!</p></div></div>
+        <div class="comment"><i class="fa-regular fa-user"></i><div class="comment_txt"><h1>Sir Nasir</h1><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit laudantium praesentium laborum assumenda officiis quis voluptatem libero porro, dicta delectus asperiores, repellat eum!</p></div></div>
+    </div>
+</div>
+
+<section class="Web_font" id="FAQ"><h1>Qna and About us</h1><p>Qna and some conversation about us</p></section>
+
+
+<div class="footer">
+    <div class="footer_elems">
+        <h1>FAQs</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus qui esse praesentium ut. Beatae ab magnam ut, nobis vero dicta.</p>
+  <div class="FAQs">
+    <div class="FAQ" data-answer="Pakistan’s cultural diversity allows tourists to experience different traditions, foods, festivals, and local lifestyles across each region. From Punjabi and Sindhi culture to Pashtun and Balochi traditions, every area offers a unique experience that makes traveling more enjoyable and meaningful.">How does Pakistan’s rich cultural diversity enhance the travel experience for visitors</div>
+    <p class="answer"></p>
+
+    <div class="FAQ" data-answer="Tourists often praise Pakistan’s hospitality because locals warmly welcome visitors, offer help, and treat guests with great respect. This friendly attitude makes travelers feel safe and connected, creating a positive and memorable experience.">Why do international travelers praise the hospitality of people in Pakistan</div>
+    <p class="answer"></p>
+
+    <div class="FAQ" data-answer="Pakistan is unique because it offers mountains, deserts, beaches, historic cities, and diverse cultures all in one country. Travelers can explore beautiful valleys like Hunza and Skardu, ancient sites like Taxila, and modern cities like Lahore and Islamabad. The warm hospitality of the people also makes the experience unforgettable.">Why is Pakistan considered one of the most unique travel destinations in Asia</div>
+    <p class="answer"></p>
+
+    <div class="FAQ" data-answer="Pakistan’s northern areas attract tourists due to their stunning landscapes, cool climate, crystal-clear lakes, and high mountain ranges. Places like Naran Kaghan, Swat, Hunza, and Fairy Meadows are known for breathtaking views and peaceful natural surroundings, making them ideal for nature lovers.">What makes Pakistan’s northern areas highly popular among tourists</div>
+    <p class="answer"></p>
+</div>
+    </div>
+</div>
+
+    <div class="aboutus">
+        <h1>About</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur pariatur nam aliquid sint, rem deleniti exercitationem quaerat tempore suscipit nostrum illo nisi laborum!</p>
+        <img src="ChatGPT Image Nov 29, 2025, 12_53_08 PM.png" alt="">
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
